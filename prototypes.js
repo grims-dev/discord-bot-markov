61a5f79240ac1cd4e85eeca035046e7e1b9c2a88
Ciaran <ciarangrimshawdev@gmail.com>
2021-03-06 13:07:26 +0000

    Update clean function with more solid punctuation regex

--- prototypes.js
+++ prototypes.js
@@ -5,7 +5,8 @@ String.prototype.tokenize = function () {
 
 // clear all non-alphanumeric characters (punctuation) and make it lowercase - fall back to removing only alphanumeric
 String.prototype.clean = function () {
-  return this.replace(/[^A-z 0-9]/g,"").toLowerCase() || this.replace(/[A-z 0-9]/g,"");
+  const punctuationRegex = /[\u2000-\u206F\u2E00-\u2E7F\\'!"#$%&()*+,\-.\/:;<=>?@\[\]^_`{|}~]/g;
+  return this.replace(punctuationRegex,"").toLowerCase() || this.replace(/[A-z 0-9]/g,"");
 }
 
 // return a random element from an array
