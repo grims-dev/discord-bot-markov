276d2c44b34c16730c5f63b1057c3507290647f5
Ciaran <ciarangrimshawdev@gmail.com>
2021-06-22 20:10:26 +0100

    Add randomResponseChance and randomResponseCooldown to config, adjust random chance, cleanup

--- discord-client.js
+++ discord-client.js
@@ -5,54 +5,63 @@ const Discord = require('discord.js');
 
 const MarkovGeneratorWord = markov.MarkovGeneratorWord;
 const bot = new MarkovGeneratorWord(2, 50);
-const botToken = discordSetup.token.trim();
-const botCommand = discordSetup.command.trim();
-const botCommandStrict = botCommand + ".strict";
+const botToken = discordSetup.token.trim() || false;
+const botCommand = discordSetup.command.trim() || false;
+const randomResponseChance = discordSetup.randomResponseChance / 100 || 0.015;
+const randomResponseCooldownMS = discordSetup.randomResponseCooldown * 1000 || 360;
+const botCommandStrict = botCommand + '.strict';
 const messageOptions = {
   disableMentions: 'everyone',
   split: true,
 };
+const codeBlockRegex = /(```)(.*?)(```)/gims;
+const whitespaceRegex = /\s\s+/g;
 let timeCheck = new Date().getTime();
+let lastAuthorId; 
 
 const client = new Discord.Client();
 
 client.login(botToken);
 
 client.on('ready', () => {
-  const lines = fs.readFileSync("log.txt").toString().split('\n');
-  if (lines.length) {
-    lines.forEach(line => {
-      bot.feed(line.trim());
-    });
-  }
+  const lines = fs.readFileSync('log.txt').toString().split('\n');
+  if (!lines.length) return;
+  lines.forEach(line => bot.feed(line.trim()));
 });
+
 
 client.on('message', message => {
   if (message.content.startsWith(botCommand)) {
     const isStrictCommand = message.content.startsWith(botCommandStrict);
     const botCommandToRemove = isStrictCommand ? botCommandStrict : botCommand;
     // remove bot command and trim whitespace
-    const userMessage = message.content.replace(botCommandToRemove, "").trim() || false;
+    const userMessage = message.content.replace(botCommandToRemove, '').trim() || false;
     // generate user message with optional search parameter
     message.channel.send(bot.generate(userMessage, isStrictCommand), messageOptions);
   } else {
     // not a request; now we do some checks to see if it's worthy of being added to file/markov chain
     // we want to ignore bots (including self!) and disallow DM input
     if (message.author.bot == false && message.guild !== null) {
-      // trim and replace double spaces, line breaks, etc
-      const userMessage = message.content.trim().replace(/\s\s+/g, ' ');
+      // trim and replace code blocks, double spaces, line breaks, etc
+      const userMessage = message.content
+        .trim()
+        .replace(codeBlockRegex, '')
+        .replace(whitespaceRegex, ' ');
 
       // only worth storing if 2 words or more
       if (userMessage.split(' ').length >= 2) {
-        fs.appendFile("log.txt", "\n" + userMessage, (err) => { if (err) console.log(err); });
+        fs.appendFile('log.txt', '\n' + userMessage, (err) => { if (err) console.log(err); });
         bot.feed(userMessage);
       }
 
-      // bonus: very small % chance of replying to non-request after certain amount of time has passed
-      if (Math.random() > 0.98 && timeCheck < new Date().getTime() - 300000) {
+      // bonus: chance of replying to non-request after certain amount of time has passed
+      const chance = message.author.id === lastAuthorId ? randomResponseChance * 0.75 : randomResponseChance;
+      if (Math.random() < chance && timeCheck < new Date().getTime() - randomResponseCooldownMS) {
         message.channel.send(bot.generate(userMessage.tokenize().choice()), messageOptions);
         timeCheck = new Date().getTime();
       }
+
+      lastAuthorId = message.author.id;
     }
   }
 });
--- discord-setup-example.js
+++ discord-setup-example.js
@@ -1,10 +1,18 @@
-// your Discord developer application's bot token: https://discordapp.com/developers/applications/me
-const token = "";
+// your Discord developer application's bot token: https://discordapp.com/developers/applications/me (string)
+const token = '';
 
-// the command you want the bot to respond to when at the start of the message
-const command = "";
+// the command you want the bot to respond to when at the start of the message (string)
+const command = '';
+
+// percentage chance to randomly respond to a message (number, 0-100)
+const randomResponseChance = 1.5;
+
+// time in seconds before another random message can be sent (number)
+const randomResponseCooldown = 360;
 
 module.exports = {
-  token: token,
-  command: command
+  token,
+  command,
+  randomResponseChance,
+  randomResponseCooldown,
 }
